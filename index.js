dd663f1b57578a8fe765797b16c4aa961d026f65
Sadman-Sakib-AIUB <sadmansakib.aiub@gmail.com>
2025-11-13 16:56:56 +0600

    Added login and find-partners api

--- index.js
+++ index.js
@@ -29,23 +29,56 @@ async function run() {
     client.connect();
     const db = client.db("studyMate");
     const partnersCollection = db.collection("partners");
+    const usersCollection = db.collection("users");
 
+    //------------------------------------- User APIs--------------------------------
+    app.post("/users", async (req, res) => {
+      const newUser = req.body;
 
-    app.post('/partners',async(req,res)=>{
-        const newPartner = req.body;
-        const result = await partnersCollection.insertOne(newPartner);
-        res.send(result);
-    })
+      const existingUser = await usersCollection.findOne({
+        email: newUser.email,
+      });
+      if (!existingUser) {
+        await usersCollection.insertOne(newUser);
+      }
+      res.send({ message: "User saved or already exists" });
+    });
+    
 
+    app.get("/users/:email", async (req, res) => {
+      const email = req.params.email;
+      const user = await usersCollection.findOne({ email });
+      if (!user) {
+        return res.status(404).send(null);
+      }
+      res.send(user);
+    });
 
+    //------------------------------------ Partners APIs ---------------------------
 
+    app.post("/partners", async (req, res) => {
+      const newPartner = req.body;
+      const result = await partnersCollection.insertOne(newPartner);
+      res.send(result);
+    });
 
+    app.get("/partners", async (req, res) => {
+      const cursor = partnersCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
 
-     client.db("admin2").command({ ping: 1 });
+    app.get("/partners/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await partnersCollection.findOne(query);
+      res.send(result);
+    });
+
+    client.db("admin2").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
-
   } finally {
   }
 }
