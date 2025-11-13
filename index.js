7cc3658aa03e0f986e0ab11c63722c2a468f1313
Sadman-Sakib-AIUB <sadmansakib.aiub@gmail.com>
2025-11-13 18:22:46 +0600

    Added new api in connection

--- index.js
+++ index.js
@@ -30,6 +30,7 @@ async function run() {
     const db = client.db("studyMate");
     const partnersCollection = db.collection("partners");
     const usersCollection = db.collection("users");
+    const connectionsCollection = db.collection("connections");
 
     //------------------------------------- User APIs--------------------------------
     app.post("/users", async (req, res) => {
@@ -43,7 +44,6 @@ async function run() {
       }
       res.send({ message: "User saved or already exists" });
     });
-    
 
     app.get("/users/:email", async (req, res) => {
       const email = req.params.email;
@@ -73,6 +73,81 @@ async function run() {
       const query = { _id: new ObjectId(id) };
       const result = await partnersCollection.findOne(query);
       res.send(result);
+    });
+
+    app.patch("/partners/:id", async (req, res) => {
+      try {
+        const id = req.params.id;
+        const { partnerCount } = req.body;
+
+        const filter = { _id: new ObjectId(id) };
+        const updateDoc = {
+          $set: {
+            partnerCount: partnerCount,
+          },
+        };
+
+        const result = await partnersCollection.updateOne(filter, updateDoc);
+        res.send(result);
+      } catch (error) {
+        console.error("Error updating partner count:", error);
+        res.status(500).send({ error: "Failed to update partner count" });
+      }
+    });
+
+    //--------------------------------------- Connections APIs -------------------------------
+    app.post("/connections", async (req, res) => {
+      const connectionData = req.body;
+
+      //prevent duplicate requests
+      const exists = await connectionsCollection.findOne({
+        partnerId: connectionData.partnerId,
+        userEmail: connectionData.userEmail,
+      });
+
+      if (exists) {
+        return res.status(400).send({ message: "Already connected!" });
+      }
+      const result = await connectionsCollection.insertOne(connectionData);
+      res.send(result);
+    });
+
+    app.get("/connections", async (req, res) => {
+      const email = req.query.email;
+      const result = await connectionsCollection
+        .find({ requesterEmail: email })
+        .toArray();
+      res.send(result);
+    });
+
+    app.delete("/connections/:id", async (req, res) => {
+      const id = req.params.id;
+      const result = await connectionsCollection.deleteOne({
+        _id: new ObjectId(id),
+      });
+      res.send(result);
+    });
+
+    app.put("/connections/:id", async (req, res) => {
+      const id = req.params.id;
+      const updatedData = req.body;
+      const result = await connectionsCollection.updateOne(
+        { _id: new ObjectId(id) },
+        { $set: updatedData }
+      );
+      res.send(result);
+    });
+
+    app.get("/connections/:email", async (req, res) => {
+      try {
+        const email = req.params.email;
+        const query = { userEmail: email };
+        const result = await connectionsCollection.find(query).toArray();
+        res.send(result);
+      } catch (error) {
+        console.error("Error fetching connections:", error);
+        res.status(500).send({ error: "Failed to fetch user connections" });
+      }
     });
 
     client.db("admin2").command({ ping: 1 });
